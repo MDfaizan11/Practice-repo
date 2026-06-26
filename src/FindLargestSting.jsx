d836626f6b86bbbaae04ad8f62e20669af456721
MDfaizan11 <khanfaizan.qk@gmail.com>
2026-06-26 22:31:57 +0530

    new file

--- src/FindLargestSting.jsx
+++ src/FindLargestSting.jsx
@@ -1,46 +1,105 @@
+// // import React from "react";
+
+// // function FindLargestSting() {
+// //   let myName = "mohammed faizan";
+// //   let num = myName.split(" ");
+// //   console.log(num);
+
+// //   const largeString = num.reduce((prev, curr) => {
+// //     return curr.length > prev.length ? curr : prev;
+// //   });
+// //   console.log(largeString);
+
+// //   const smallString = num.reduce((prev, curr) => {
+// //     return curr.length < prev.length ? curr : prev;
+// //   });
+// //   console.log(smallString);
+
+// //   let largestSting = "";
+// //   for (let large of num) {
+// //     if (large.length > largestSting.length) {
+// //       largestSting = large;
+// //     }
+// //   }
+// //   console.log(largestSting);
+
+// //   let smallestString = num[0];
+// //   for (let small of num) {
+// //     if (small.length < smallestString.length) {
+// //       smallestString = small;
+// //     }
+// //   }
+// //   console.log(smallestString);
+
+// //   function ReverseName(word) {
+// //     console.log(word);
+// //     // let result = word.reverse().join(" ");
+// //     // console.log(result);
+// //     let result = word.map((item, index) => {
+// //       return item.split("").reverse().join(" ");
+// //     });
+// //     console.log(result.join(" , "));
+// //   }
+// //   ReverseName(num);
+// //   return <div>FindLargestSting</div>;
+// // }
+
+// // export default FindLargestSting;
+
 // import React from "react";
 
 // function FindLargestSting() {
-//   let myName = "mohammed faizan";
-//   let num = myName.split(" ");
-//   console.log(num);
+//   let myname = "mohammed faizaan";
+//   let newName = myname.toLowerCase().split(" ");
+//   console.log(newName);
 
-//   const largeString = num.reduce((prev, curr) => {
-//     return curr.length > prev.length ? curr : prev;
+//   const result = newName.reduce((prev, curr) => {
+//     if (curr.length > prev.length) {
+//       return curr;
+//     } else {
+//       return prev;
+//     }
 //   });
-//   console.log(largeString);
+//   console.log(result);
 
-//   const smallString = num.reduce((prev, curr) => {
-//     return curr.length < prev.length ? curr : prev;
-//   });
-//   console.log(smallString);
+//   let largestString = "";
 
-//   let largestSting = "";
-//   for (let large of num) {
-//     if (large.length > largestSting.length) {
-//       largestSting = large;
+//   for (let large of newName) {
+//     if (large.length > largestString.length) {
+//       largestString = large;
 //     }
 //   }
-//   console.log(largestSting);
+//   console.log(largestString);
 
-//   let smallestString = num[0];
-//   for (let small of num) {
-//     if (small.length < smallestString.length) {
+//   let smallestString = newName[0];
+//   for (let small of newName) {
+//     if (small < smallestString) {
 //       smallestString = small;
 //     }
 //   }
 //   console.log(smallestString);
 
-//   function ReverseName(word) {
-//     console.log(word);
-//     // let result = word.reverse().join(" ");
-//     // console.log(result);
-//     let result = word.map((item, index) => {
-//       return item.split("").reverse().join(" ");
-//     });
-//     console.log(result.join(" , "));
+//   const name = "mohammed shabaz";
+//   let num = name.split(" ");
+//   let largestString2 = "";
+
+//   for (let large of num) {
+//     if (large.length > largestString2.length) {
+//       largestString2 = large;
+//     }
 //   }
-//   ReverseName(num);
+//   console.log(largestString2);
+
+//   function fingLarge(word) {
+//     let large = word[0];
+//     for (let i = 0; i < word.length; i++) {
+//       if (word[i].length > large.length) {
+//         large = word[i];
+//       }
+//     }
+//     console.log(large)
+//   }
+//   fingLarge(num);
 //   return <div>FindLargestSting</div>;
 // }
 
@@ -49,11 +108,9 @@
 import React from "react";
 
 function FindLargestSting() {
-  let myname = "mohammed faizaan";
-  let newName = myname.toLowerCase().split(" ");
-  console.log(newName);
-
-  const result = newName.reduce((prev, curr) => {
+  const name = "mohammed faizan";
+  const newname = name.split(" ");
+  const result = newname.reduce((prev, curr) => {
     if (curr.length > prev.length) {
       return curr;
     } else {
@@ -63,43 +120,21 @@ function FindLargestSting() {
   console.log(result);
 
   let largestString = "";
-
-  for (let large of newName) {
+  for (let large of newname) {
     if (large.length > largestString.length) {
       largestString = large;
     }
   }
   console.log(largestString);
 
-  let smallestString = newName[0];
-  for (let small of newName) {
-    if (small < smallestString) {
-      smallestString = small;
+  let smallest = newname[0];
+  for (let small of newname) {
+    if (small.length < smallest.length) {
+      smallest = small;
     }
   }
-  console.log(smallestString);
+  console.log(smallest);
 
-  const name = "mohammed shabaz";
-  let num = name.split(" ");
-  let largestString2 = "";
-
-  for (let large of num) {
-    if (large.length > largestString2.length) {
-      largestString2 = large;
-    }
-  }
-  console.log(largestString2);
-
-  function fingLarge(word) {
-    let large = word[0];
-    for (let i = 0; i < word.length; i++) {
-      if (word[i].length > large.length) {
-        large = word[i];
-      }
-    }
-    console.log(large)
-  }
-  fingLarge(num);
   return <div>FindLargestSting</div>;
 }
 
--- src/FindMissElement.jsx
+++ src/FindMissElement.jsx
@@ -1,33 +1,67 @@
+// // function FindMissElement() {
+// //   const number = [1, 2, 3, 5, 6, 7, 8, 9];
+// //   function findMiss(number) {
+// //     const totalLength = number.length + 1;
+// //     console.log(totalLength);
+// //     const expectedSum = (totalLength * (totalLength + 1)) / 2;
+// //     console.log(expectedSum);
+// //     const arraysum = number.reduce((prev, curr) => curr + prev, 0);
+// //     console.log(arraysum);
+// //     const result = expectedSum - arraysum;
+// //     console.log(result);
+// //   }
+// //   findMiss(number);
+// //   let arrnum = [1, [2, 3, [4, 5]]];
+// //   const flatenarray = arrnum.flat(Infinity);
+// //   console.log(flatenarray);
+
+// //   function FlatenByLoop(num) {
+// //     let result = [];
+// //     for (let array of num) {
+// //       if (Array.isArray(array)) {
+// //         result.push(...FlatenByLoop(array));
+// //       } else {
+// //         result.push(array);
+// //       }
+// //     }
+// //     console.log(result);
+// //     return result;
+// //   }
+// //   FlatenByLoop(arrnum);
+// //   return <div>FindMissElement</div>;
+// // }
+
+// // export default FindMissElement;
+
+// import React from "react";
+
 // function FindMissElement() {
-//   const number = [1, 2, 3, 5, 6, 7, 8, 9];
-//   function findMiss(number) {
-//     const totalLength = number.length + 1;
-//     console.log(totalLength);
-//     const expectedSum = (totalLength * (totalLength + 1)) / 2;
-//     console.log(expectedSum);
-//     const arraysum = number.reduce((prev, curr) => curr + prev, 0);
-//     console.log(arraysum);
-//     const result = expectedSum - arraysum;
+//   let array = [1, 2, 4, 5, 6, 7,8, 9];
+//   function findMiss(num) {
+//     let arrayLenght = num.length + 1;
+//     console.log(arrayLenght);
+//     let expectesum = (arrayLenght * (arrayLenght + 1)) / 2;
+//     console.log(expectesum);
+
+//     let arraySum = num.reduce((prev, curr) => curr + prev, 0);
+//     console.log(arraySum);
+//     let result = expectesum - arraySum;
 //     console.log(result);
 //   }
-//   findMiss(number);
-//   let arrnum = [1, [2, 3, [4, 5]]];
-//   const flatenarray = arrnum.flat(Infinity);
-//   console.log(flatenarray);
+//   findMiss(array);
 
-//   function FlatenByLoop(num) {
+//   function twomiss(num) {
+//     let max = Math.max(...num);
 //     let result = [];
-//     for (let array of num) {
-//       if (Array.isArray(array)) {
-//         result.push(...FlatenByLoop(array));
-//       } else {
-//         result.push(array);
+//     for (let i = 1; i <= max; i++) {
+//       if (!num.includes(i)) {
+//         result.push(i);
 //       }
 //     }
+
 //     console.log(result);
-//     return result;
 //   }
-//   FlatenByLoop(arrnum);
+//   twomiss(array);
 //   return <div>FindMissElement</div>;
 // }
 
@@ -36,32 +70,13 @@
 import React from "react";
 
 function FindMissElement() {
-  let array = [1, 2, 4, 5, 6, 7,8, 9];
-  function findMiss(num) {
-    let arrayLenght = num.length + 1;
-    console.log(arrayLenght);
-    let expectesum = (arrayLenght * (arrayLenght + 1)) / 2;
-    console.log(expectesum);
+  const array = [1, 2, 4, 5, 6, 7, 8, 9];
 
-    let arraySum = num.reduce((prev, curr) => curr + prev, 0);
-    console.log(arraySum);
-    let result = expectesum - arraySum;
-    console.log(result);
-  }
-  findMiss(array);
-
-  function twomiss(num) {
-    let max = Math.max(...num);
-    let result = [];
-    for (let i = 1; i <= max; i++) {
-      if (!num.includes(i)) {
-        result.push(i);
-      }
-    }
-
-    console.log(result);
-  }
-  twomiss(array);
+  const arrayLength = array.length + 1;
+  const expectedSum = (arrayLength * (arrayLength + 1)) / 2;
+  const originalArraySum = array.reduce((prev, acc) => prev + acc);
+  const missedNumber = expectedSum - originalArraySum;
+  console.log(missedNumber);
   return <div>FindMissElement</div>;
 }
 
